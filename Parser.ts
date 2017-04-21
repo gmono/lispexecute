dcca5ff1a19473012baf7259115909de65b8fb38
高子健 <gaozijian0411@outlook.com>
2017-04-21 19:33:50 +0800

    基本完善

--- Parser.ts
+++ Parser.ts
@@ -1,13 +1,102 @@
 namespace LispExecute
 {
     /**
+     * 此为一个结构体 用于Parse过程
+     */
+         interface ReadStruct
+        {
+            obj:Table;
+            nowptr:number;
+        }
+    /**
      * 此为解析工具包对外公开几个静态函数 用于将lisp代码转换为表
      */
     export class Parser
     {
+
         public static Parse(code:string):Table
         {
-            return new Table();
+            //递归实现
+            //跳过第一个括号
+            let nowptr=0;
+            for(;nowptr<code.length&&(code[nowptr]==' '||code[nowptr]=='\t');++nowptr);
+            //这里应该指向code的末尾或者第一个非空字符
+            //如果这个非空字符不是表的开始 就返回空
+            if(code.length==nowptr||code[nowptr]!='(') return null;
+            nowptr++;//指向括号后面
+           let ret=this.ParseCode(code,0);
+           if(ret==null) return null;
+           return ret.obj;
+        }
+        /**
+         * 字面量读取函数
+         * @param val 字面量
+         */
+        protected static ReadValue(val:string):Table
+        {
+
+        }
+        /**
+         * 解析代码的递归函数 约定：返回位置总是在此函数读取的最后一个字符后面
+         * 总是从ptr指向的位置开始读取
+         * @param code 代码
+         * @param ptr  当前读取位置
+         */
+        protected static ParseCode(code:string,ptr:number):ReadStruct
+        {
+            let container=new Table();
+            //扫描
+            //当前字面量缓存
+            let nowval:string="";
+            let isinread=false;
+            let isok=false;
+            for(;ptr<code.length;++ptr)
+            {
+                let c=code[ptr];
+                if(c==" "||c=="\t")
+                {
+                    //这里逻辑为 如果在读取状态遇到空格意味着读取结束 
+                    //否则直接跳过
+                    if(isinread)
+                    {
+                        let obj=this.ReadValue(nowval);
+                        container.childs.push(obj);
+                        nowval="";
+                        isinread=false;
+                    }
+                    else continue;
+                }
+                else if(c=="(")
+                {
+                    //递归调用读取一个子复合表
+                    let res=this.ParseCode(code,ptr+1);
+                    //从新的位置开始读取
+                    ptr=res.nowptr;
+                    container.childs.push(res.obj);
+                }
+                else if(c==")")
+                {
+                    //如果遇到表结束
+                    if(isinread)
+                    {
+                        let obj=this.ReadValue(nowval);
+                        container.childs.push(obj);
+                    }
+                    //统一 出循环后应指向读到的最后一个字符后面
+                    ptr++;
+                    //标识为正常结束
+                    isok=true;
+                    break;
+                }
+                else
+                {
+                    //普通字面量
+                    if(!isinread) isinread=true;
+                    nowval+=c;
+                }
+            }
+            if(!isok) return null;
+            return <ReadStruct>{nowptr:ptr,obj:container};
         }
     }
 }
