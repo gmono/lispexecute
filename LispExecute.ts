bcf053938b9a2e7ce66d070070b297d4d2fbf643
高子健 <gaozijian0411@outlook.com>
2017-04-21 19:10:04 +0800

    运行器完成

--- LispExecute.ts
+++ LispExecute.ts
@@ -4,6 +4,9 @@ namespace LispExecute
     {
         key:string;
         val:any;
+        //下面是函数专属属性
+        isneedcircum?:boolean;
+        callthis?:any;
     }
     /**
      * 提供顶层环境和外部接口
@@ -25,32 +28,58 @@ namespace LispExecute
          */
         public SetSymbol(sym:SymPair)
         {
-                 //如果val是object就转换为Table
-                if(sym.val instanceof Table)
-                {
-                    //如果是表就直接加入 否则封装为LispObject
-                    this.TopContainer.set(sym.key,sym.val);
-                }
-                else
-                {
-                    let lobj=new LispObject(sym.val);
-                    this.TopContainer.set(sym.key,lobj);
-                }
+            this.TopContainer.set(sym.key,this.ToTable(sym));
         }
         /**
          * 获取一个符号代表的变量 自动类型转换
          * @param name 获取的变量的名字
          */
-        public GetSymbol(name:string)
+        public GetSymbol(name:string):any
         {
             if(!this.TopContainer.has(name)) return null;
             let obj=this.TopContainer.get(name);
+            return this.ToRaw(obj);
+
+        }
+        public ToRaw(obj:Table)
+        {
             if(obj.Type=="object")
             {
                 let oobj=obj as LispObject;
                 return oobj.Object;
             }
+            else if(obj instanceof LispRawProcess)
+            {
+                let fun=obj as LispRawProcess;
+                return fun.rawFunc;
+            }
+            //不需要转换就直接返回
             return obj;
+        }
+        /**
+         * 此处sympair中仅val和函数专属的属性有效key可以为null
+         * @param sym 符号 key无用
+         */
+        public ToTable(sym:SymPair)
+        {
+                             //如果val是object就转换为Table
+                if(sym.val instanceof Table)
+                {
+                    //如果是表就直接加入 否则封装为LispObject
+                    return sym.val;
+                }
+                else if(typeof sym.val=="function")
+                {
+                    //封装函数
+                    let fun:Function=sym.val as Function;
+                    let func=new LispRawProcess(fun.name,fun,sym.isneedcircum,sym.callthis);
+                    return func;
+                }
+                else
+                {
+                    let lobj=new LispObject(sym.val);
+                    return lobj;
+                }
         }
         public Run(obj:Table)
         {
